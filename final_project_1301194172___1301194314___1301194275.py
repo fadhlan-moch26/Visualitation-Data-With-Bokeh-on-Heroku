e5de234b3181cc2c4704c1904ab95a1e75ee4a21
fadhlan-moch26 <85607446+fadhlan-moch26@users.noreply.github.com>
2022-01-10 12:17:10 +0700

    Update final_project_1301194172___1301194314___1301194275.py

--- final_project_1301194172___1301194314___1301194275.py
+++ final_project_1301194172___1301194314___1301194275.py
@@ -1,87 +1,91 @@
 import pandas as pd
-from bokeh.io import curdoc
 from bokeh.plotting import figure
-from bokeh.models import HoverTool, ColumnDataSource
-from bokeh.models import CategoricalColorMapper
-from bokeh.palettes import Spectral6
-from bokeh.layouts import widgetbox, row, gridplot
-from bokeh.models import Slider, Select
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns 
+from bokeh.plotting import show
+from bokeh.io import curdoc
+from bokeh.models import ColumnDataSource, HoverTool, Select
+from bokeh.models.widgets import Tabs, Panel
+from bokeh.layouts import row, widgetbox
+from bokeh.palettes import Category20_16
+from bokeh.models.widgets import CheckboxGroup, Slider, RangeSlider, Tabs
+from bokeh.layouts import column, row, WidgetBox
 
-# Read dataset
-df = pd.read_csv("country_wise_latest.csv")
-df.head(5)
+df = pd.read_csv("./data/covid_19_indonesia_time_series_all.csv")
+df["Date"] = pd.to_datetime(df["Date"])
+data = df[df["Location"].str.contains("Indonesia")==False]
+data = data[['Date', 'Location', 'Total Cases', 'Total Deaths', 'Total Recovered', 'Total Active Cases']]
 
-data = df[["Country/Region", "Confirmed", "Deaths", "Recovered", "Active", "New cases", "New deaths", "New recovered", "Confirmed last week", "WHO Region"]]
-data.rename(columns={'Country/Region':'Negara', 'Confirmed':'Terkonfirmasi', 'Deaths':'Kasus_Kematian', 'Recovered':'Sembuh', 'Active':'Aktif', 'New cases':'Kasus_Baru', 'New recovered':'Baru_Pulih', 'New deaths':'Kasus_Kematian_Baru', 'Confirmed last week':'Terkonfirmasi_Minggu_Lalu', 'WHO Region':'Benua'}, inplace=True)
-data.set_index('Negara', inplace=True)
+lokasi = list(data.Location.unique())
 
-benua_list = data.Benua.unique().tolist()
+col_list = list(data.columns)
 
-mapper = CategoricalColorMapper(factors=benua_list, palette=Spectral6)
+def make_dataset(lokasi, feature):
 
-# Make the ColumnDataSource: source
-source = ColumnDataSource(data={
-    'x'       : data.Terkonfirmasi,
-    'y'       : data.Sembuh,
-    'Kasus Kematian' : data.Kasus_Kematian,
-    'Aktif'     : data.Aktif,
-    'Benua'  : data.Benua,
-})
-
-# Create the figure: plot
-plot = figure(title='1970', x_axis_label='Terkonfirmasi (children per woman)', y_axis_label='Kasus_Kematian Expectancy (years)',
-           plot_height=400, plot_width=700, tools=[HoverTool(tooltips='@Negara')])
-
-# Add a circle glyph to the figure p
-plot.circle(x='x', y='y', source=source, fill_alpha=0.8,
-           color=dict(field='Benua', transform=mapper), legend='Benua')
-
-# Set the legend and axis attributes
-plot.legend.location = 'top_left'
-
-# Define the callback function: update_plot
-def update_plot(attr, old, new):
-    # set the `negara` name to `slider.value` and `source.data = new_data`
-    x = x_select.value
-    y = y_select.value
-    # Label axes of plot
-    plot.xaxis.axis_label = x
-    plot.yaxis.axis_label = y
-    # new data
-    new_data = {
-    'x'       : data.loc[x],
-    'y'       : data.loc[y],
-    'Kasus Kematian' : data.Kasus_Kematian,
-    'Aktif'     : data.Aktif,
-    'Benua'  : data.Benua,
-    }
-    source.data = new_data
     
-    # Add title to figure: plot.title.text
-    plot.title.text = 'Gapminder data for %d' % negara
+    xs = []
+    ys = []
+    colors = []
+    labels = []
 
-# Make dropdown menu for x and y axis
-# Create a dropdown Select widget for the x data: x_select
-x_select = Select(
-    options=['Terkonfirmasi', 'Kasus_Kematian', 'Sembuh', 'Aktif'],
-    value='Terkonfirmasi',
-    title='x-axis data'
-)
-# Attach the update_plot callback to the 'value' property of x_select
-x_select.on_change('value', update_plot)
+    for i, lokasi in enumerate(lokasi):
 
-# Create a dropdown Select widget for the y data: y_select
-y_select = Select(
-    options=['Terkonfirmasi', 'Kasus_Kematian', 'Sembuh', 'Aktif'],
-    value='Kasus_Kematian',
-    title='y-axis data'
-)
-# Attach the update_plot callback to the 'value' property of y_select
-y_select.on_change('value', update_plot)
+        df = data[data['Location'] == lokasi].reset_index(drop = True)
+        
+        x = list(df['Date'])
+        y = list(df[feature])
+        
+        xs.append(list(x))
+        ys.append(list(y))
+
+        colors.append(Category20_16[i])
+        labels.append(lokasi)
+
+    new_src = ColumnDataSource(data={'x': xs, 'y': ys, 'color': colors, 'label': labels})
+
+    return new_src
+
+def make_plot(src, feature):
     
-# Create layout and add to current document
-layout = row(widgetbox(x_select, y_select), plot)
+    p = figure(plot_width = 700, plot_height = 400, 
+            title = 'Covid19-Indonesia All Time Series',
+            x_axis_label = 'Date', y_axis_label = 'Feature Selected')
+
+    p.multi_line('x', 'y', color = 'color', legend_field = 'label', line_width = 2, source = src)
+
+    return p
+
+def update_country(attr, old, new):
+    lokasi_plot = [lokasi_selection.labels[i] for i in lokasi_selection.active]
+
+    
+    new_src = make_dataset(lokasi_plot, feature_select.value)
+
+    src.data.update(new_src.data)
+
+def update_feature(attr, old, new):
+    lokasi_plot = [lokasi_selection.labels[i] for i in lokasi_selection.active]
+    
+    feature = feature_select.value
+    
+    new_src = make_dataset(lokasi_plot, feature)
+
+    src.data.update(new_src.data)
+
+country_selection = CheckboxGroup(labels=lokasi, active = [0])
+country_selection.on_change('active', update_country)
+
+feature_select = Select(options = col_list[2:], value = 'Total Cases', title = 'Feature Select')
+feature_select.on_change('value', update_feature)
+
+initial_country = [country_selection.labels[i] for i in country_selection.active]
+
+src = make_dataset(initial_country, feature_select.value)
+
+p = make_plot(src, feature_select.value)
+
+controls = WidgetBox(feature_select, country_selection)
+
+# Create a row layout
+layout = row(controls, p)
+
+#Adding the layout to the current document
 curdoc().add_root(layout)
